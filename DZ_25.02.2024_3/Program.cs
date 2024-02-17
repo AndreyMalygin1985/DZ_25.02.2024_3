2175196448967b228e752c85a8478821f7d1d80d
Андрей <dusha22075@gmail.com>
2024-02-17 15:13:29 +0300

    Добавьте файлы проекта.

---
+++ DZ_25.02.2024_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры четыре цифры. Необходимо создать число,
+// содержащее эти цифры. Например, если с клавиатуры введено
+// 1, 5, 7, 8 тогда нужно сформировать число 1578.
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите четыре цифры (от 1 до 9, через Enter):");
+
+        int digit1 = Convert.ToInt32(Console.ReadLine());
+        int digit2 = Convert.ToInt32(Console.ReadLine());
+        int digit3 = Convert.ToInt32(Console.ReadLine());
+        int digit4 = Convert.ToInt32(Console.ReadLine());
+
+        int number = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+
+        Console.WriteLine("Получившиеся число: " + number);
+    }
+}
